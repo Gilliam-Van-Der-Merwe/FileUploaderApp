ecd5bb19fe05d1879cd3493e2efeffacbc93fb10
Gilliam Van Der Merwe <gilliam007@gmail.com>
2025-03-10 20:50:05 +0200

    Update: Clean up

--- FileUploadingApplication/Program.cs
+++ FileUploadingApplication/Program.cs
@@ -2,17 +2,6 @@ using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder
-    .Services
-    .Scan(
-        selector => selector
-            .FromAssemblies(
-                Infrastructure.AssemblyReference.Assembly,
-                Persistence.AssemblyReference.Assembly)
-            .AddClasses(false)
-            .AsImplementedInterfaces()
-            .WithScopedLifetime());
-
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Application.AssemblyReference.Assembly));
 
 builder
